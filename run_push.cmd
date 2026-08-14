ee41415f689e4c33197be69e423710b15b0ad2f4
TruongAnh2706 <ductruong.onl@gmail.com>
2026-08-14 07:21:39 +0700

    Release v2.3.3: Fixed Inno Setup Action via Choco for 100 CI/CD Build Success

--- run_push.cmd
+++ run_push.cmd
@@ -1,12 +1,12 @@
 @echo off
-title DTA Studio - Release v2.3.2 Trigger Engine
+title DTA Studio - Release v2.3.3 Trigger Engine
 cd /d "d:\DTA VideoUnify"
-echo Pushing Release v2.3.2 to GitHub...
+echo Pushing Release v2.3.3 to GitHub...
 git add .
-git commit -m "Release v2.3.2: Ultra-Fast Direct Copy Concat Engine and Fixed GitHub Release Action"
-git tag -f v2.3.2
-git push origin main v2.3.2 -f
-git push core main v2.3.2 -f
+git commit -m "Release v2.3.3: Fixed Inno Setup Action via Choco for 100% CI/CD Build Success"
+git tag -f v2.3.3
+git push origin main v2.3.3 -f
+git push core main v2.3.3 -f
 echo.
-echo HOAN TAT! F5 TRANG GITHUB ACTIONS HOAC BAM UPDATES TREN APP DE NHAN BAN V2.3.2!
+echo HOAN TAT! F5 TRANG GITHUB ACTIONS HOAC BAM UPDATES TREN APP DE NHAN BAN V2.3.3!
 pause
